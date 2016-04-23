ae151237f0f8c80648fa5c19e36cf3546ee68d56
ys1224 <ys1224@nyu.edu>
2016-04-23 18:03:15 -0400

    week 6 update

--- week 6 rotation.js
+++ week 6 rotation.js
@@ -1,20 +1,31 @@
-function setup(){
-  createCanvas(400, 400);
- 
+var angle=0.0
+
+function setup() {
+  createCanvas(400,400); 
+  background('white')
 }
-function draw() {
-  noStroke();
-  fill(0,0,0,50);
-  translate(frameCount%width,frameCount%height);
-  rotate(radians(frameCount));
-  rect(0,0,100,100)
-  translate(frameCount%width,frameCount%height);
-  rotate(radians(frameCount));
-  ellipse(200,200,100,100)
-  translate(frameCount%width,frameCount%height);
-  rotate(radians(frameCount));
-  ellipse(300,300,100,100)
+
+function draw(){
+  translate(200,200);
+  rotate(angle)
+  mine(0,0);
+  angle+=0.1
+  
+  translate(100,100);
+  rotate(angle)
+  mine(0,0);
+  angle+=0.1
+  
+  
+    
+  
 }
-    function mousePressed(){
-  clear();
-    }
+
+function mine(x,y){
+  fill('black')
+  ellipse(x,y,50,50);
+  fill('white')
+  noStroke()
+  rect(x-20,y-20,40,40)
+}
+
